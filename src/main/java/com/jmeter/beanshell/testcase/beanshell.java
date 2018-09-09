b5183f4c6443d3d5a0b0670302403ee41bee1161
74543804duan <437765664@qq.com>
2018-09-09 18:32:19 +0800

    ...

--- src/main/java/com/jmeter/beanshell/testcase/beanshell.java
+++ src/main/java/com/jmeter/beanshell/testcase/beanshell.java
@@ -1,22 +1,32 @@
 package com.jmeter.beanshell.testcase;
 
-
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class beanshell{
-	
-	
-	public static void main(String[] args) {
-		//加密
-		System.out.println("*****加密*****");
-		String password = "123123";
-		//调用工具类中的方法进行加密
-		String encode = SecurityUtils.getKey(password);
-		System.out.println("Set my encode");
-		//把值保存到jmeter变量encode中
-		vars.put("encode",encode);
-		String getEncode=vars.get("encode");
-		System.out.println("Get my encode: " + getEncode);
-	}
 
+	
+	
+	public void getdate() {
+		try {
+			//获取当前时间
+			Date date = new Date();
+			SimpleDateFormat SD = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss SSS");
+			String nowDate = SD.format(date);
+			//通过日历获取明天的日期
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(SD.parse(nowDate));
+			calendar.add(calendar.DAY_OF_YEAR, +3);
+			String chanceDate = SD.format(calendar.getTime());
+			calendar.add(calendar.DAY_OF_YEAR, +7);
+			String planFinishDate = SD.format(calendar.getTime());
+			vars.put("chanceDate",chanceDate);
+			vars.put("planFinishDate",planFinishDate);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		
+}
 }
